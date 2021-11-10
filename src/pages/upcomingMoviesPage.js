86f0f0cf2a558d9f671a612da128cb364a774081
Stephen Carbery <carbery.stephen@gmail.com>
2021-11-10 19:08:25 +0000

    Upcoming movies page was changed in the previous commit to use caching.

--- src/pages/upcomingMoviesPage.js
+++ src/pages/upcomingMoviesPage.js
@@ -3,26 +3,7 @@ import PageTemplate from '../components/templateMovieListPage'
 import { getUpcoming } from "../api/tmdb-api";
 import { useQuery } from 'react-query'
 import Spinner from '../components/spinner'
-import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
 import AddToWatchlistIcon from "../components/cardIcons/addToWatchlist";
-// const UpcomingMoviesPage = (props) => {
-//   const [movies, setMovies] = useState([]);
-//   const favorites = movies.filter(m => m.favorite)
-//   localStorage.setItem('favorites', JSON.stringify(favorites))
-
-//   const addToFavorites = (movieId) => {
-//     const updatedMovies = movies.map((m) =>
-//       m.id === movieId ? { ...m, favorite: true } : m
-//     );
-//     setMovies(updatedMovies);
-//   };
-
-//   useEffect(() => {
-//     getUpcoming().then(movies => {
-//       setMovies(movies);
-//     });
-//     // eslint-disable-next-line react-hooks/exhaustive-deps
-//   }, []);
 
 const UpcomingMoviesPage = (props) => {
   const {  data, error, isLoading, isError }  = useQuery('upcoming', getUpcoming)
